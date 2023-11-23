126c4e93c611db8ec08f92bfea478b05d1726fc6
TheCodingZone <anuragtiwari14447@gamil.com>
2023-11-24 00:29:53 +0530

    first commit

---
+++ src/pages/NewsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsPage = () => {
+  return (
+    <h3>
+      This Is News Page.
+    </h3>
+  )
+}
+
+export default NewsPage
---
+++ src/pages/aboutus.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const aboutus = () => {
+  return (
+    <h3>
+      This Is About Us Page.
+    </h3>
+  )
+}
+
+export default aboutus
